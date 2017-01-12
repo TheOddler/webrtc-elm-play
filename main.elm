8deb430ddadf8e53228a57d8e11bb66002568821
Pablo Bollansée <pablo.bollansee@gmail.com>
2017-01-12 20:03:01 +0100

    Added beginning of chess.

--- main.elm
+++ main.elm
@@ -3,6 +3,7 @@ import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import WebRTC exposing (..)
 import Chat exposing (..)
+import Game exposing (..)
 
 main = Html.program
     { init = init
@@ -15,14 +16,19 @@ main = Html.program
 -- MODEL
 type alias Model = 
     { chat : Chat.Model
+    , game : Game.Model
     }
     
 init : (Model, Cmd Msg)
-init = (Model Chat.init, Cmd.none)
+init = (Model Chat.init Game.init, Cmd.none)
 
 
 -- UPDATE
-type Msg = Test | Received WebRTC.Message | ForChat Chat.Msg
+type Msg 
+    = Test 
+    | Received WebRTC.Message 
+    | ForChat Chat.Msg 
+    | ForGame Game.Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -40,6 +46,11 @@ update msg model =
                 (chatModel, chatCmd) = Chat.update msg model.chat
             in  
                 ({ model | chat = chatModel}, Cmd.map ForChat chatCmd)
+        ForGame msg ->
+            let 
+                (gameModel, gameCmd) = Game.update msg model.game
+            in  
+                ({ model | game = gameModel}, Cmd.map ForGame gameCmd)
         
 
 -- SUBSCRIPTIONS
@@ -48,6 +59,7 @@ subscriptions model =
     Sub.batch 
         [ WebRTC.listen Received
         , Sub.map ForChat <| Chat.subscriptions model.chat
+        , Sub.map ForGame <| Game.subscriptions model.game
         ]
 
 
@@ -56,4 +68,5 @@ view : Model -> Html Msg
 view model =
     div []
         [ Html.map ForChat (Chat.view model.chat)
+        , Html.map ForGame (Game.view model.game)
         ]
