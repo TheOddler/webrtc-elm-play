91a81b7faa031c9dafa20cee36421026f8d44b88
Pablo Bollansée <pablo.bollansee@gmail.com>
2017-02-09 21:17:09 +0100

    Pieces added (currently all drawn as circles).

--- Chess/View.elm
+++ Chess/View.elm
@@ -1,9 +1,12 @@
 module Chess.View exposing (..)
 
-import Chess.Model exposing (..)
 import Html exposing ( Html )
+import Dict exposing (Dict, toList)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+
+import Chess.Model exposing (..)
+import Chess.Piece as Piece exposing (..)
 
 -- VIEW
 type Config msg = Config
@@ -15,15 +18,29 @@ view : Config msg -> Model -> Html msg
 view (Config config) model =
     svg
       [ class "chess", width "256", height "256", viewBox "0 0 256 256" ]
-      [ viewBoard 256 ]
+      [ viewBoard 256, viewPieces 256 model.pieces ]
+
+viewPieces : Int -> Dict Position Piece -> Svg msg
+viewPieces size pieces =
+    node "svg" [class "pieces"] 
+        <| List.map (viewPiece <| toFloat size / 8) <| Dict.toList pieces
+
+viewPiece : Float -> (Position, Piece) -> Svg msg
+viewPiece size ((row, col), piece) = 
+    let
+        (xPos, yPos) = toCoordinatesString size row col
+        s = toString size
+    in 
+        rect [ class "piece", x xPos, y yPos, width s, height s, rx s, ry s ] []
 
 viewBoard : Int -> Svg msg
 viewBoard size =
     let 
         vdc = viewDarkCell (toFloat size / 8)
+        s = toString size
     in 
         node "svg" [class "board"]
-            [ rect [ class "back", x "0", y "0", width <| toString size, height <| toString size ] []
+            [ rect [ class "back", x "0", y "0", width s, height s ] []
             , vdc 7 1, vdc 7 3, vdc 7 5, vdc 7 7 --dark cells
             , vdc 6 0, vdc 6 2, vdc 6 4, vdc 6 6
             , vdc 5 1, vdc 5 3, vdc 5 5, vdc 5 7
@@ -37,13 +54,13 @@ viewBoard size =
 viewDarkCell : Float -> Int -> Int -> Svg msg
 viewDarkCell size row col =
     let
-        (xPos, yPos) = toCoordinates size row col
+        (xPos, yPos) = toCoordinatesString size row col
     in
         rect 
             [ class "dark"
-            , x <| toString xPos, y <| toString yPos
+            , x xPos, y yPos
             , width <| toString size, height <| toString size 
             ] []
 
-toCoordinates : Float -> Int -> Int -> (Float, Float)
-toCoordinates size row col = (toFloat col * size, 7 * size - toFloat row * size)
+toCoordinatesString : Float -> Int -> Int -> (String, String)
+toCoordinatesString size row col = (toString <| toFloat col * size, toString <| 7 * size - toFloat row * size)
