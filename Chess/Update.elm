3820cf2404da00f6704fe6bb5acc174ab071de99
Pablo Bollansée <pablo.bollansee@gmail.com>
2017-02-11 12:30:04 +0100

    Quick hacked moveable pieces. No rule-checking, no webrtc.

--- Chess/Update.elm
+++ Chess/Update.elm
@@ -1,5 +1,7 @@
 module Chess.Update exposing (..)
 
+import Dict exposing (get, remove, insert)
+
 import Chess.Model exposing (..)
 import Chess.View exposing (..)
 import WebRTC
@@ -8,13 +10,13 @@ import WebRTC
 type Msg 
     = Ignore
     | Click Position
-    | FinalizeMove Position Position
+    | DoMove Position Position
 
 
 createConfig : (Msg -> msg) -> Config msg
 createConfig wrap = Config
     { click = wrap << Click
-    , finalizeMove = (\from to -> wrap <| FinalizeMove from to)
+    , finalizeMove = (\from to -> wrap <| DoMove from to)
     }
 
 update : Msg -> Model -> (Model, Cmd msg)
@@ -22,24 +24,40 @@ update msg model =
     case msg of
         Ignore -> (model, Cmd.none)
         
-        Click pos -> (model, Cmd.none)
-            {-case model.move of
-                Nothing -> ({ model | move = Selected pos}, Cmd.none)
+        Click pos ->
+            case model.state of
+                Waiting -> ({ model | state = PieceSelected pos}, Cmd.none)
 
-                Selected sel -> 
+                PieceSelected sel -> 
                     if sel == pos
-                    then ({ model | move = Nothing}, Cmd.none)
-                    else ({ model | move = PreviewMove sel pos}, Cmd.none)
+                    then ({ model | state = Waiting}, Cmd.none)
+                    else ({ model | state = PreviewMove sel pos}, Cmd.none)
 
                 PreviewMove from to ->
-                    if from == pos then ({ model | move = Nothing}, Cmd.none)
+                    if from == pos then ({ model | state = Waiting}, Cmd.none)
                     else if to == pos then 
-                        let (newModel, newCmd) = update (FinalizeMove from to) model
+                        let 
+                            (newModel, newCmd) = update (DoMove from to) model
                             -- TODO Also do the webrtc send
-                        in (newModel, newCmd)
-                    else ({ model | move = PreviewMove from pos}, Cmd.none)
-                -}
-        FinalizeMove from to -> (model, Cmd.none)
+                        in 
+                            (newModel, newCmd)
+                    else ({ model | state = Waiting}, Cmd.none)
+        
+        DoMove from to -> 
+            let
+                curPieces = model.pieces
+                taken = get to curPieces
+                moved = get from curPieces
+                newPieces = 
+                    case moved of
+                        Just m  -> insert to m (remove from curPieces)
+                        Nothing -> curPieces
+                newGraveyard =
+                    case taken of
+                        Just t -> t :: model.graveyard
+                        Nothing -> model.graveyard
+            in
+               ({model | pieces = newPieces, graveyard = newGraveyard, state = Waiting }, Cmd.none)
 
 
 
--- Chess/View.elm
+++ Chess/View.elm
@@ -4,6 +4,7 @@ import Html exposing ( Html, div, ul, li )
 import Dict exposing (Dict, toList)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Svg.Events exposing (..)
 
 import Chess.Model exposing (..)
 import Chess.Piece as Piece exposing (..)
@@ -15,7 +16,7 @@ type Config msg = Config
     }
 
 view : Config msg -> Model -> Html msg
-view (Config config) model =
+view config model =
     let 
         size = 300
         sizeText = toString size
@@ -23,61 +24,63 @@ view (Config config) model =
         div [class "chess"]
             [ svg
                 [ width sizeText, height sizeText, viewBox <| "0 0 " ++ sizeText ++ " " ++ sizeText ]
-                [ viewBoard size
-                , viewPieces size model.pieces
+                [ viewBoard config size
+                , viewPieces config size model.pieces
                 , text "Your browser doesn't support svg :("
                 ]
             , viewGraveyard size model.graveyard
             ]
 
-viewPieces : Int -> Dict Position Piece -> Svg msg
-viewPieces size pieces =
+viewPieces : Config msg -> Int -> Dict Position Piece -> Svg msg
+viewPieces config size pieces =
     node "svg" [class "pieces"] 
-        <| List.map (viewPiece <| toFloat size / 8) <| Dict.toList pieces
+        <| List.map (viewPiece config <| toFloat size / 8) <| Dict.toList pieces
 
-viewPiece : Float -> (Position, Piece) -> Svg msg
-viewPiece size ((row, col), piece) = 
+viewPiece : Config msg -> Float -> (Position, Piece) -> Svg msg
+viewPiece (Config cnfg) size ((row, col), piece) = 
     let
         (baseX, baseY) = toCoordinates size row col
         xPos = toString <| baseX + size / 2
         yPos = toString <| baseY + size - size / 10
         s = toString size
     in 
-        text_ [ class "piece", x xPos, y yPos, fontSize s, textAnchor "middle" ] [ text <| Piece.toText piece ]
+        text_ [ class "piece", onClick <| cnfg.click (row, col), x xPos, y yPos, fontSize s, textAnchor "middle" ] [ text <| Piece.toText piece ]
+
+viewBoard : Config msg -> Int -> Svg msg
+viewBoard config size =
+    let 
+        vc = viewCell config (toFloat size / 8)
+        even = (\row col -> vc (if col % 2 == 0 then "dark cell" else "light cell") row col)
+        odd = (\row col -> vc (if col % 2 == 1 then "dark cell" else "light cell") row col)
+        cols = List.range 0 7
+        s = toString size
+    in 
+        node "svg" [class "board"]
+            <| [ rect [ class "back", x "0", y "0", width s, height s ] [] ]
+            ++ (List.map (even 0) cols)
+            ++ (List.map (odd  1) cols)
+            ++ (List.map (even 2) cols)
+            ++ (List.map (odd  3) cols)
+            ++ (List.map (even 4) cols)
+            ++ (List.map (odd  5) cols)
+            ++ (List.map (even 6) cols)
+            ++ (List.map (odd  7) cols)
+
+viewCell : Config msg -> Float -> String -> Int -> Int -> Svg msg
+viewCell (Config cnfg) size class_ row col =
+    let
+        (xPos, yPos) = toCoordinates size row col
+    in
+        rect [ class class_
+             , onClick <| cnfg.click (row, col)
+             , x <| toString xPos, y <| toString yPos
+             , width <| toString size, height <| toString size 
+             ] []
 
 viewGraveyard : Float -> List Piece -> Html msg
 viewGraveyard size graveyard =
     ul  [ class "graveyard" ]
         <| List.map (\p -> li [] [ text <| Piece.toText p ] ) graveyard
-
-viewBoard : Int -> Svg msg
-viewBoard size =
-    let 
-        vdc = viewDarkCell (toFloat size / 8)
-        s = toString size
-    in 
-        node "svg" [class "board"]
-            [ rect [ class "back", x "0", y "0", width s, height s ] []
-            , vdc 7 1, vdc 7 3, vdc 7 5, vdc 7 7 --dark cells
-            , vdc 6 0, vdc 6 2, vdc 6 4, vdc 6 6
-            , vdc 5 1, vdc 5 3, vdc 5 5, vdc 5 7
-            , vdc 4 0, vdc 4 2, vdc 4 4, vdc 4 6
-            , vdc 3 1, vdc 3 3, vdc 3 5, vdc 3 7
-            , vdc 2 0, vdc 2 2, vdc 2 4, vdc 2 6
-            , vdc 1 1, vdc 1 3, vdc 1 5, vdc 1 7
-            , vdc 0 0, vdc 0 2, vdc 0 4, vdc 0 6
-            ]
-
-viewDarkCell : Float -> Int -> Int -> Svg msg
-viewDarkCell size row col =
-    let
-        (xPos, yPos) = toCoordinates size row col
-    in
-        rect 
-            [ class "dark"
-            , x <| toString xPos, y <| toString yPos
-            , width <| toString size, height <| toString size 
-            ] []
 
 toCoordinates : Float -> Int -> Int -> (Float, Float)
 toCoordinates size row col = (toFloat col * size, 7 * size - toFloat row * size)
