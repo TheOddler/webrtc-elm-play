8c3c92db2a78bac64b761ad7771055f9bd1d1359
Pablo Bollansée <pablo.bollansee@gmail.com>
2017-02-11 10:43:42 +0100

    Cleaned code.

--- Chess/Piece.elm
+++ Chess/Piece.elm
@@ -1,19 +1,11 @@
 module Chess.Piece exposing (..)
 
-import Html exposing ( Html, div, span, text, input, button, table, tr, td )
-import Html.Attributes as A exposing ( class, id )
-
-
 type PieceType = King | Queen | Rook | Bishop | Knight | Pawn
 type Color = Black | White
 type alias Piece = 
     { color : Color
     , figure : PieceType
     }
-
-toDiv : Piece -> Html msg
-toDiv piece =
-    div [ class "piece" ] [ text <| toText piece ]
 
 toText : Piece -> String
 toText piece =
